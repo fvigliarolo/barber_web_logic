2347c07dc1450a4ed2c441570289b5e254c02809
fvigliarolo <57096219+fvigliarolo@users.noreply.github.com>
2024-01-03 23:26:07 -0300

    Delete componentes directory

--- componentes/src/utils/barber_variables.js
+++
@@ -1,6 +0,0 @@
-import { getFetch  } from "../utils/fetch.js";
-let barberos = {c05ada79dbe25: "alex", bf791a441a922:"Facu", df65fcd88bbe: "fede" }
-const servicio = "00:30:00"
-const barbers_basic_info = getFetch('barberos/data');
-
-export  {barberos, servicio, barbers_basic_info};
